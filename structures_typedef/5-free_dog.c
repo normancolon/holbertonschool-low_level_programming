f58da3642ee670c075b24b9fabc1689586a96130
LAB <lab@kali>
2023-11-10 09:18:09 -0500

    f

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees a dog structure
+ * @d: A pointer to the struct dog to free
+ */
+void free_dog(dog_t *d)
+{
+    if (d != NULL)
+    {
+        free(d->name);
+        free(d->owner);
+        free(d);
+    }
+}
+
