39a57376f48157754a1ffd11310a67aa267298a0
LAB <lab@kali>
2023-12-02 21:04:38 -0500

    b

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,7 +2,6 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
-
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output.
  * @filename: The name of the file to be read.
@@ -15,15 +14,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	ssize_t n_read, n_written;
 	char *buffer;
-
 	if (filename == NULL)
 		return (0);
-
 	/* Open the file */
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
 	/* Allocate memory for the buffer */
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
@@ -31,7 +27,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	/* Read from the file */
 	n_read = read(fd, buffer, letters);
 	if (n_read == -1)
@@ -40,7 +35,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	/* Print to the standard output */
 	n_written = write(STDOUT_FILENO, buffer, n_read);
 	if (n_written == -1 || n_written != n_read)
@@ -49,10 +43,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	/* Free the buffer and close the file */
 	free(buffer);
 	close(fd);
-
 	return (n_written);
 }
---
+++ file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * create_file - creates a file and writes a text content to it.
+ * @filename: the name of the file to create.
+ * @text_content: a NULL terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t len = 0, wr;
+
+	if (filename == NULL)
+		return (-1);
+
+	/* Open the file with appropriate flags and permissions */
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	/* If text_content is not NULL, write it to the file */
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+		wr = write(fd, text_content, len);
+		if (wr == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
+
