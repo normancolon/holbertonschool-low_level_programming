c1d04816e806171bdf648fd5cafb1dbe9edfe40f
LAB <lab@kali>
2023-12-02 21:15:00 -0500

    c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -11,31 +11,31 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t len = 0, wr;
+	int fd;
+	ssize_t len = 0, wr;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    /* Open the file with append mode */
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return (-1);
+	/* Open the file with append mode */
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
 
-    /* If text_content is NULL, don't append anything but check if file exists */
-    if (text_content != NULL)
-    {
-        while (text_content[len])
-            len++;
-        wr = write(fd, text_content, len);
-        if (wr == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+	/* If text_content is NULL, don't append anything but check if file exists */
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+		wr = write(fd, text_content, len);
+		if (wr == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-    close(fd);
-    return (1);
+	close(fd);
+	return (1);
 }
 
