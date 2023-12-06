98c0660e3bf345eb047028963b3eead1ccff450e
LAB <lab@kali>
2023-12-06 09:57:11 -0500

    a

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -7,14 +7,17 @@
  *
  * Return: A pointer to the newly created hash table, or NULL if an error occurs.
  */
- 
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
 	unsigned long int i;
+
+	/* Allocate memory for hash table structure */
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
+
+	/* Initialize the elements of the hash table */
 	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
@@ -22,7 +25,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(hash_table);
 		return (NULL);
 	}
+
+	/* Initialize each cell of the array to NULL */
 	for (i = 0; i < size; i++)
 		hash_table->array[i] = NULL;
+
 	return (hash_table);
 }
